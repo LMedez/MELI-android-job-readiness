0f90114952a04b0eb4640e5777bdeba9bec287fa
lumenendez <luciano.menendez@mercadolibre.com>
2022-06-10 14:55:17 -0300

    initial project setup

---
+++ app/src/main/java/com/luc/meli_job_readiness/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.luc.meli_job_readiness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
